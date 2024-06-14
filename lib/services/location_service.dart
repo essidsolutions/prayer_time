ce627c521462fd2b2884942a11b6f83919efa438
Mohamed Chedly Essid <mcessid@icloud.com>
2024-06-14 16:20:44 +0300

    Update HomeScreen to use new Prayer model

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geocoding/geocoding.dart';
 
 class LocationService with ChangeNotifier {
   Position? _currentPosition;
@@ -16,20 +15,10 @@ class LocationService with ChangeNotifier {
   Future<void> getCurrentLocation() async {
     try {
       _currentPosition = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      await _getCityNameFromCoordinates(_currentPosition!);
+      // Implement logic to get city name from coordinates
+      _cityName = 'Dummy City';
     } catch (e) {
       // Handle error
-    }
-    notifyListeners();
-  }
-
-  Future<void> _getCityNameFromCoordinates(Position position) async {
-    try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
-      Placemark place = placemarks[0];
-      _cityName = place.locality ?? 'Unknown city';
-    } catch (e) {
-      _cityName = 'Unknown city';
     }
     notifyListeners();
   }
